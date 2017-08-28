39f273790c88ed903c5d918e14a76a910a0e2b82
Tom Sid <tom_sid@yahoo.com>
2017-08-28 13:03:49 -0500

    Part-02-03 - @RequestParam usage

--- onlineshopping/src/main/java/com/example/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/example/onlineshopping/controller/PageController.java
@@ -2,6 +2,7 @@ package com.example.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,6 +11,13 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("page");
 		modelAndView.addObject("greeting", "Welcome to the world of spring MVC!");
+		return modelAndView;
+	}
+
+	@RequestMapping({ "/test" })
+	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("greeting", greeting);
 		return modelAndView;
 	}
 }
