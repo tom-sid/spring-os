c87fb2b76467bdb64a9ac525f1323d7f81a981bd
Tom Sid <tom_sid@yahoo.com>
2017-08-28 18:11:32 -0500

    Part-02-06 - UI design

--- onlineshopping/src/main/java/com/example/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/example/onlineshopping/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.example.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,21 +9,24 @@ public class PageController {
 	@RequestMapping({ "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("page");
-		modelAndView.addObject("greeting", "Welcome to the world of spring MVC!");
+		modelAndView.addObject("title", "Home");
+		modelAndView.addObject("userClickHome", true);
 		return modelAndView;
 	}
-
-//	@RequestMapping({ "/test" })
-//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
-//		ModelAndView modelAndView = new ModelAndView("page");
-//		modelAndView.addObject("greeting", greeting);
-//		return modelAndView;
-//	}
 	
-	@RequestMapping({ "/test/{greeting}" })
-	public ModelAndView test(@PathVariable("greeting") String greeting) {
+	@RequestMapping({ "/about" })
+	public ModelAndView about() {
 		ModelAndView modelAndView = new ModelAndView("page");
-		modelAndView.addObject("greeting", greeting);
+		modelAndView.addObject("title", "About Us");
+		modelAndView.addObject("userClickAbout", true);
+		return modelAndView;
+	}
+	
+	@RequestMapping({ "/contact" })
+	public ModelAndView contact() {
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("title", "Contact Us");
+		modelAndView.addObject("userClickContact", true);
 		return modelAndView;
 	}
 }
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,14 @@
+$(function() {
+	// Solving the active menu problem
+	switch (menu) {
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	default:
+		$('#home').addClass('active');
+		break;
+	}
+});
